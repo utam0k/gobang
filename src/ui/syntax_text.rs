b462f3df01d5296a427a5237591489f8fc9f65c1
Takayuki Maeda <takoyaki0316@gmail.com>
2021-09-09 12:27:35 +0900

    add syntax text component

--- src/ui/syntax_text.rs
+++ src/ui/syntax_text.rs
@@ -1,4 +1,4 @@
-use std::{ops::Range, path::Path};
+use std::ops::Range;
 use syntect::{
     highlighting::{
         FontStyle, HighlightState, Highlighter, RangedHighlightIterator, Style, ThemeSet,
@@ -18,8 +18,8 @@ pub struct SyntaxText {
 
 impl SyntaxText {
     pub fn new(text: String) -> Self {
-        const SYNTAX_SET: SyntaxSet = SyntaxSet::load_defaults_nonewlines();
-        const THEME_SET: ThemeSet = ThemeSet::load_defaults();
+        let SYNTAX_SET: SyntaxSet = SyntaxSet::load_defaults_nonewlines();
+        let THEME_SET: ThemeSet = ThemeSet::load_defaults();
 
         let mut state = ParseState::new(SYNTAX_SET.find_syntax_by_extension("sql").unwrap());
         let highlighter = Highlighter::new(&THEME_SET.themes["base16-eighties.dark"]);
@@ -42,6 +42,25 @@ impl SyntaxText {
             text,
             lines: syntax_lines,
         }
+    }
+
+    pub fn convert(&self) -> tui::text::Text<'_> {
+        let mut result_lines: Vec<Spans> = Vec::with_capacity(self.lines.len());
+
+        for (syntax_line, line_content) in self.lines.iter().zip(self.text.lines()) {
+            let mut line_span = Spans(Vec::with_capacity(syntax_line.items.len()));
+
+            for (style, _, range) in &syntax_line.items {
+                let item_content = &line_content[range.clone()];
+                let item_style = syntact_style_to_tui(style);
+
+                line_span.0.push(Span::styled(item_content, item_style));
+            }
+
+            result_lines.push(line_span);
+        }
+
+        result_lines.into()
     }
 }
 
